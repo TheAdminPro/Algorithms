b6cf9e5ee5fd0fd103610b1d2ef44ab250d63075
TheAdminPro <45514836+TheAdminPro@users.noreply.github.com>
2019-12-04 20:17:35 +0300

    Bubble Sort

---
+++ bubbleSort.js
@@ -0,0 +1,10 @@
+const bubbleSort = list => {
+    for(let i = list.length-1; i >= 0; i--){
+        for (let j = 0; j < i; j++){
+            if (list[j] > list[j+1]){
+                [list[j], list[j+1]] = [list[j+1], list[j]];
+            }
+        }
+    }
+    return list;
+}
