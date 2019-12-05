8e03aaf5b2006116883c9a050be62fd8bd0a0e94
TheAdminPro <45514836+TheAdminPro@users.noreply.github.com>
2019-12-05 14:37:27 +0300

    Bubble Sort Modified

--- bubbleSort.js
+++ bubbleSort.js
@@ -3,7 +3,7 @@
  * @return {Array<number>} list - sorted array
  */
 const bubbleSort = list => {
-    for(let i = list.length-1; i >= 0; i--){
+    for(let i = list.length-1; i > 0; i--){
         for (let j = 0; j < i; j++){
             if (list[j] > list[j+1]){
                 [list[j], list[j+1]] = [list[j+1], list[j]];
